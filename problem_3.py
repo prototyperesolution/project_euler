c551b40573bca6000eedb22702d504856cd6183b
Joe <86804949+prototyperesolution@users.noreply.github.com>
2023-05-08 21:35:30 +0100

    commit

--- problem_3.py
+++ problem_3.py
@@ -1,15 +1,12 @@
 def find_factors(n):
-    factors = []
     d = 2
     while n > 1:
         while n % d == 0:
             n /= d
-            factors.append(d)
         d += 1
         if d*d > n:
             if n != 1:
-                factors.append(int(n))
+                return int(n)
                 break
-    return factors[-1]
 
 print(find_factors(600851475143))
---
+++ problem_4.py
@@ -0,0 +1,1 @@
+def larget_palindrome():
